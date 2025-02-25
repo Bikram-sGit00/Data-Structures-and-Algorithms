7c66acfe48f88739c45eacd064c55d36b43a7d33
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-25 17:59:21 +0530

    pattern10

--- Mastering_patterns/patterns.cpp
+++ Mastering_patterns/patterns.cpp
@@ -156,9 +156,23 @@ void pattern9(int n)
         cout << endl;
     }
 }
+void pattern10(int n)
+{
+    for (int i = 0; i <= 2 * n - 1; i++)
+    {
+        int stars = i;
+        if (i > n)
+            stars = 2 * n - i;
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     int n = 5;
-    pattern9(n);
+    pattern10(n);
     return 0;
 }
