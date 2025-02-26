7a30c74977b1c97e73bc01aca7b03207323bddb8
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-26 19:54:51 +0530

    p17

--- 02_Mastering_patterns/patterns.cpp
+++ 02_Mastering_patterns/patterns.cpp
@@ -258,9 +258,40 @@ void pattern16(int n)
         cout << endl;
     }
 }
+void pattern17(int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        // charecters
+        char ch = 'A';
+        int breakPoint = (2 * i + 1) / 2;
+        for (int j = 1; j <= 2 * i + 1; j++)
+        {
+            cout << ch << " ";
+            if (j <= breakPoint)
+                ch++;
+            else
+                ch--;
+        }
+
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     int n = 5;
-    pattern16(n);
+    pattern17(n);
     return 0;
 }
