953a58ef6fbda82c453be88bd7f55d65e8afa4a0
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-26 12:56:39 +0530

    p13

--- 02_Mastering_patterns/patterns.cpp
+++ 02_Mastering_patterns/patterns.cpp
@@ -211,9 +211,22 @@ void pattern12(int n)
         spaces -= 2;
     }
 }
+void pattern13(int n)
+{
+    int num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num += 1;
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     int n = 5;
-    pattern10(n);
+    pattern13(n);
     return 0;
 }
