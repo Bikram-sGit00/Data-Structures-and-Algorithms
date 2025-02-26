f39efc7d0a3bc8729814280dcff58c9465d74961
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-26 11:39:56 +0530

    pattern12

--- 02_Mastering_patterns/patterns.cpp
+++ 02_Mastering_patterns/patterns.cpp
@@ -170,6 +170,30 @@ void pattern10(int n)
         cout << endl;
     }
 }
+void pattern11(int n)
+{
+    int start = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0)
+            start = 1;
+        else
+            start = 0;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << start << " ";
+            start -= start;
+        }
+        cout << endl;
+    }
+}
+void pattern12(int n)
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+}
 int main()
 {
     int n = 5;
