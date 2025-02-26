6ca5ca5f1e60d0ce8a48506c6edc16202f0c3e6f
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-26 17:33:38 +0530

    p15

--- 02_Mastering_patterns/patterns.cpp
+++ 02_Mastering_patterns/patterns.cpp
@@ -235,6 +235,18 @@ void pattern14(int n)
         cout << endl;
     }
 }
+void pattern15(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + (n - i); ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n = 5;
