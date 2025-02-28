bd7e16e6bc0c3215e47efaa822ac820b285183f7
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-02-28 21:09:50 +0530

    understanding vectors

--- 03_STL/stl.cpp
+++ 03_STL/stl.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h> // This will include all standard libraries
 using namespace std;
 
-void pair() // utility library
+void understandingPairs() // utility library
 {
     pair<int, double> p = {1, 3.27}; // pair of two integers into a single entity
     cout << p.first << " " << p.second << endl;
@@ -13,8 +13,27 @@ void pair() // utility library
     cout << arr[0].first << " " << arr[0].second << endl;
 }
 
+void understandingVectors()
+{
+    // vectors are containers that can store multiple values of same type
+    // We need vectors cause arrays are constant/static in size
+    vector<int> v;
+    v.push_back(1);    // adding elements to the vector
+    v.emplace_back(2); // similar to push_back but more efficient or fast as it constructs the element in place rather than copying it
+
+    vector<pair<int, int>> vec;
+    vec.push_back({1, 2});  // adding pair of integers to the vector
+    vec.emplace_back(3, 4); // no need to use curly braces
+
+    vector<int> var1(5);     // vector of size 5 with all elements as 0 or any garbage value depends on the compiler
+    vector<int> var2(5, 10); // vector of size 5 with all elements as 10
+
+    vector<int> v1(5, 20);
+    vector<int> v2(v1); // copy of v1
+}
 int main()
 {
+    cout << "Compiled🚀✨";
 
     return 0;
 }
