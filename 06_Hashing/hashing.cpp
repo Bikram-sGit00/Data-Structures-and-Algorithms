945b463ceb9367a8a91c2d092d086441c67d5d99
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-03-08 15:48:25 +0530

    add hashing implementation to count occurrences of numbers

---
+++ 06_Hashing/hashing.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // precompute:
+    int hash[13] = {0}; // assuming that the max size we will get is 12 so we initialized 13 spaces fill with 0s
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]] += 1;
+    }
+
+    int query;
+    cin >> query;
+    while (query--)
+    {
+        int number;
+        cin >> number;
+        cout << hash[number] << endl;
+    }
+    return 0;
+}
