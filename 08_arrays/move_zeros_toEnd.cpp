6c9d4d4bc186cb4c4de91f04173ad8f9e2cfa1e3
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-04-01 01:02:24 +0530

    moves zeros to end

---
+++ 08_arrays/move_zeros_toEnd.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    public:
+        void moveZeroes(vector<int>& arr) {
+            int n=arr.size();
+            int j=0;
+            for(int i=0;i<n;i++){
+                if(arr[i]==0){
+                    j=i;
+                    break;
+                }
+            }
+            // if(j==-1) 
+            for(int i=j+1;i<n;i++){
+                if(arr[i]!=0){
+                    if(n>2){
+                        swap(arr[i],arr[j]);
+                    }
+                    j++;
+                }
+            }
+        }
+    };
