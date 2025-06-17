ce16bddb71f3a57eefc0d5bed4a49ede5e18f158
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-06-17 13:18:25 +0530

    refactor longestSuccessiveElements function for improved readability

--- 08_arrays/Medium/11_longestConsecutiveSequenceInArr.cpp
+++ 08_arrays/Medium/11_longestConsecutiveSequenceInArr.cpp
@@ -12,20 +12,22 @@
 
 int longestSuccessiveElements(vector<int>&a) {
     int n = a.size(); //size of array
-    int longest = 1;
+    int longest = 1;  //Assuming the minimum length of consecutive sequence is 1
     //pick a element and search for its
     //consecutive numbers:
     for (int i = 0; i < n; i++) {
-        int x = a[i];
-        int cnt = 1;
+        int x = a[i]; //current element
+        int cnt = 1; //count of consecutive numbers starting from x
+
         //search for consecutive numbers
         //using linear search:
+        
         while (linearSearch(a, x + 1) == true) {
-            x += 1;
-            cnt += 1;
+            x += 1; //increment x to check for next consecutive number
+            cnt += 1; //increment count of consecutive numbers
         }
 
-        longest = max(longest, cnt);
+        longest = max(longest, cnt); // Update longest if current count is greater
     }
     return longest;
 }
