e4cc8990f8c87f45690351f98b47d35ff9a2cf3a
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-06-21 12:44:30 +0530

    add company tags to nextPermutation.cpp

--- 08_arrays/Medium/09_nextPermutation.cpp
+++ 08_arrays/Medium/09_nextPermutation.cpp
@@ -55,3 +55,5 @@ public:
 // Time Complexity: O(N)
 // Space Complexity: O(1) or O(N) if we consider the input vector as additional space, but since we are modifying it in place, we can consider it O(1).
 // Note: The use of reverse is more efficient than sorting the remaining elements, as it runs in O(N) time complexity compared to O(N log N) for sorting.
+
+Company Tags --> Infosys Flipkart Amazon Microsoft FactSet Hike MakeMyTrip Google Qualcomm Salesforce
