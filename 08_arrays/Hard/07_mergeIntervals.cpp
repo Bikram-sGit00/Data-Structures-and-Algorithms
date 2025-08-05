aab21ea539157ccd12cbddedd2614a63ff3376b9
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-08-05 20:59:56 +0530

    Refactor merge intervals solution to optimize merging process using a single loop

--- 08_arrays/Hard/07_mergeIntervals.cpp
+++ 08_arrays/Hard/07_mergeIntervals.cpp
@@ -30,8 +30,26 @@ public:
 // Time Complexity: O(nlogn) for sorting + O(2N) you might think it is O(n^2) but it is not because we are breaking the loop when we find a non-overlapping interval
 // Space Complexity: O(n) for storing the merged intervals
 
-✅ Better Approach -->  Instead of using a nested loop, we can optimize the merging process by keeping track of the last merged interval and only adding a new interval if it doesn't overlap
+✅ Optimized Approach -->  We can further optimize the merging process by using a single loop and maintaining the start and end of the current merged interval
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        vector<vector<int>> ans;
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        for (int i = 0; i < n; i++) {
+            if (ans.empty() ||
+                arr[i][0] > ans.back()[1]) { // means new interval
+                ans.push_back(arr[i]);
+            } else { // lying in current inrterval
+                ans.back()[1] = max(ans.back()[1], arr[i][1]);
+            }
+        }
+        return ans;
+    }
+};
 
-✅ Optimized Approach --> 
+// Time Complexity: O(nlogn) for sorting + O(n) for merging 
+// Space Complexity: O(n) for storing the merged intervals
 
-✅ Company Tags -->  
+✅ Company Tags -->  Amazon Microsoft Google Nutanix Zoho
