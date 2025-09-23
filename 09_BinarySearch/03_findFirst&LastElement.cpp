9f25a1cd23220c90769ba6977c74550551424318
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-09-23 19:29:10 +0530

    solution for find first and last occuring index

--- 09_BinarySearch/03_findFirst&LastElement.cpp
+++ 09_BinarySearch/03_findFirst&LastElement.cpp
@@ -2,8 +2,46 @@
 
 ✅ Brute Force -->  Iterate through the array and find the first and last occurrence of the target element.
 
-✅ Better Approach --> 
+✅ Optimized Approach --> int lowerBound(vector<int>& arr, int target) {
+    int n = arr.size();
+    int low = 0, high = n - 1, ans = n;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] >= target) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
 
-✅ Optimized Approach --> 
+int upperBound(vector<int>& arr, int target) {
+    int n = arr.size();
+    int low = 0, high = n - 1, ans = n;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] > target) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& arr, int x) {
+        int n = arr.size();
+        int lb = lowerBound(arr, x);
+        if (lb == n || arr[lb] != x)  // checking if element is not in the array...
+            return {-1, -1};
+        return {lb, upperBound(arr, x) - 1};
+    }
+};
+
 
 ✅ Company Tags -->  Amazon
