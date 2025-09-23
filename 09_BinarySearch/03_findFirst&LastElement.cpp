f341ba3bde4dcecaf1eef134b7c08df51293301c
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-09-23 18:59:18 +0530

    init

---
+++ 09_BinarySearch/03_findFirst&LastElement.cpp
@@ -0,0 +1,9 @@
+➡️ problemLinks --> https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/ && https://www.geeksforgeeks.org/problems/first-and-last-occurrences-of-x3116/1
+
+✅ Brute Force -->  Iterate through the array and find the first and last occurrence of the target element.
+
+✅ Better Approach --> 
+
+✅ Optimized Approach --> 
+
+✅ Company Tags -->  Amazon
