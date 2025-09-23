8a417c59d1b202c62a44d515292152e672da12df
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-09-23 12:14:59 +0530

    Add floor and ceil functions for binary search operations

--- 09_BinarySearch/02_someOperations.cpp
+++ 09_BinarySearch/02_someOperations.cpp
@@ -85,4 +85,44 @@ public:
 // Time Complexity --> O(log base2^N) or O(log2(N))
 // Space Complexity --> O(1)
 
+✅ Floor --> largest element smaller than or equal to x
 
+int findFloor(int arr[], int n, int x) {
+	int low = 0, high = n - 1;
+	int ans = -1;
+
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		// maybe an answer
+		if (arr[mid] <= x) {
+			ans = arr[mid];  // or ans = mid; if index is needed
+			//look for smaller index on the left
+			low = mid + 1;
+		}
+		else {
+			high = mid - 1; // look on the right
+		}
+	}
+	return ans;
+}
+
+
+✅ Ceil --> smallest element greater than or equal to x
+int findCeil(int arr[], int n, int x) {
+	int low = 0, high = n - 1;
+	int ans = -1;
+
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		// maybe an answer
+		if (arr[mid] >= x) {
+			ans = arr[mid];
+			//look for smaller index on the left
+			high = mid - 1;
+		}
+		else {
+			low = mid + 1; // look on the right
+		}
+	}
+	return ans;
+}
