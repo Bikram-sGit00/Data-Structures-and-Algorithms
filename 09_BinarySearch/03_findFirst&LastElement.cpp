80f007f4b845560435f7c695b833c6fcd049d9ca
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-10-09 20:33:22 +0530

    Add functions to count occurrences of an element in a sorted array

--- 09_BinarySearch/03_findFirst&LastElement.cpp
+++ 09_BinarySearch/03_findFirst&LastElement.cpp
@@ -95,4 +95,47 @@ public:
 
 
 
-✅ Count occurrences of a number in a sorted array with duplicates -->
+✅ Count occurrences of a number in a sorted array with duplicates -->     
+int findFirst(vector<int>& arr, int x) {
+    int low = 0, high = arr.size() - 1;
+    int ans = -1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == x) {
+            ans = mid;
+            high = mid - 1; // keep searching left
+    } else if (arr[mid] < x) {
+            low = mid + 1;
+    } else {
+            high = mid - 1;
+    }
+    }
+        return ans;
+}
+
+    int findLast(vector<int>& arr, int x) {
+        int low = 0, high = arr.size() - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == x) {
+                ans = mid;
+                low = mid + 1; // keep searching right
+            } else if (arr[mid] < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+class Solution {
+  public:
+    int countFreq(vector<int>& arr, int target) {
+    int first = findFirst(arr, target);
+    if (first == -1) return 0; // frequency is 0 if not found
+    int last = findLast(arr, target);
+    return (last - first +1);
+    }
+};
