a2382b57821c76eacac1578e68db2343e35b7af7
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-10-10 21:31:46 +0530

    Implement optimized binary search approach for searching in a rotated sorted array

--- 09_BinarySearch/04_searchInRotatedSortedArray_I.cpp
+++ 09_BinarySearch/04_searchInRotatedSortedArray_I.cpp
@@ -13,6 +13,37 @@ public:
 // Time Complexity --> O(N)
 // Space Complexity --> O(1)
 
-✅ Optimized Approach --> 
+✅ Optimized Approach --> class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // if mid points the target
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target <= arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (arr[mid] <= target && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+// Time Complexity --> O(logN)
+// Space Complexity --> O(1)
 
 ✅ Company Tags -->   Paytm Flipkart Amazon Microsoft Snapdeal D-E-Shaw FactSet Hike MakeMyTrip Intuit Adobe Google BankBazaar Times Internet
