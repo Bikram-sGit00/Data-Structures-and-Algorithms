d1f3366acd99faee96f1a0cee45366a916c39f00
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-10-12 12:51:18 +0530

    Add function to find the number of rotations in a rotated sorted array

--- 09_BinarySearch/06_findMinimumInRotatedSortedArray.cpp
+++ 09_BinarySearch/06_findMinimumInRotatedSortedArray.cpp
@@ -29,4 +29,46 @@ public:
 // Time Complexity: O(log base2 N) in average case and O(N) in worst case
 // Space Complexity: O(1)
 
-✅ Company Tags -->  
+➡️ problem --> Find out how many times has an array been rotated (similar to this problem) 
+
+✅ Optimized Approach --> class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        int n = arr.size();
+        int low = 0;
+        int high = n - 1;
+        int ans = INT_MAX;
+        int index =-1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if(arr[low]<=arr[high]){ // check case if whole array is sorted
+              if(arr[low]<ans){
+                  ans=arr[low];
+                  index=low;
+              }
+              break;
+            }
+            if (arr[low]<=arr[mid]){
+              if(arr[low]<ans){
+                ans=arr[low];
+                index=low;
+              }
+              low=mid+1;
+            }
+            else{
+              if(arr[mid]<ans){
+                  ans=arr[mid];
+                  index=mid;
+              }
+              high=mid-1;
+            }
+        }
+        return index;
+    }
+};
+
+// Time Complexity: O(log base2 N) in average case and O(N) in worst case
+// Space Complexity: O(1)
+
+
+✅ Company Tags -->  Flipkart Amazon ABCO
