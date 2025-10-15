6cf77d1619d86abdff771f425b0c48fc81de9ba6
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-10-15 08:32:43 +0530

    Add brute force solution for calculating square root of a number

---
+++ 09_BinarySearch/onAnswers/sqrt(x).cpp
@@ -0,0 +1,18 @@
+➡️ problemLinks --> https://leetcode.com/problems/sqrtx/
+
+✅ Brute Force -->  class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0) return 0;
+        for(long long i = 1; i <= x; i++){
+            if(i * i == x) return i;
+            if(i * i > x) return i - 1;
+        }
+        return -1;
+    }
+};
+
+
+✅ Optimized Approach --> 
+
+✅ Company Tags -->  VMWare Flipkart Accolite Amazon Microsoft
