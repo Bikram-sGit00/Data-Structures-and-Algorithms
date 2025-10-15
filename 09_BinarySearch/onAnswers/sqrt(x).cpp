7e45060e0d6b47d791acdd10097244735d16e3c0
Bikram-sGit00 <ContactBikram1@gmail.com>
2025-10-15 08:37:41 +0530

    Refactor optimized approach for calculating square root using binary search

--- 09_BinarySearch/onAnswers/sqrt(x).cpp
+++ 09_BinarySearch/onAnswers/sqrt(x).cpp
@@ -13,6 +13,25 @@ public:
 };
 
 
-✅ Optimized Approach --> 
+✅ Optimized Approach --> class Solution {
+public:
+    int mySqrt(int x) {
+        int low = 1;
+        int high = x;
+        while (low <= high) {
+            long long mid = (static_cast<long long>(low) + high) / 2;
+            long long ans = (mid * mid);
+            if (ans <= x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return high;
+    }
+};
+
+// Time Complexity --> O(log n)
+// Space Complexity --> O(1)
 
 ✅ Company Tags -->  VMWare Flipkart Accolite Amazon Microsoft
