f94a092f1b05021caec85bd6ff69bcd84d892502
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-01-15 00:52:01 +0530

    Add optimized binary search approach for finding median of two sorted arrays

--- 09_BinarySearch/onAnswers/12_medianOfSortedArray.cpp
+++ 09_BinarySearch/onAnswers/12_medianOfSortedArray.cpp
@@ -104,5 +104,46 @@ double median(vector<int>& a, vector<int>& b) {
 // Space Complexity: O(1)
 
 ✅ Optimized Approach --> 
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if (n1 > n2)
+            return findMedianSortedArrays(
+                nums2, nums1); // as we always want nums1 to be small
+        int low = 0;
+        int high = n1;
+        int left = (n1 + n2 + 1) / 2;
+        int total = n1 + n2;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN; // if they are not there
+            int r1 = INT_MAX, r2 = INT_MAX; // if they are not there
+            if (mid1 < n1)
+                r1 = nums1[mid1];
+            if (mid2 < n2)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+            if (l1 <= r2 && l2 <= r1) {
+                if (total % 2 == 1) { // if odd
+                    return max(l1, l2);
+                }
+                return ((double)max(l1, l2) + (double)min(r1, r2)) / 2.0;
+            } else if (l1 > r2)
+                high = mid1 - 1;
+            else
+                low = mid1 + 1;
+        }
+        return 0;
+    }
+};
 
-✅ Company Tags -->  
+// Time Complexity: O(log(min(n1,n2))), we are applying binary search on the range [0, min(n1, n2)].
+// Space Complexity: O(1) , only constant variables are used.
+
+✅ Company Tags -->  Amazon Microsoft Samsung Google
