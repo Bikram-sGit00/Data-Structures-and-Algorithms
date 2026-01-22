a6ac33bc0898445581087d9e2ee33d0f2514b7be
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-01-22 20:53:28 +0530

    add function to delete tail

--- 10_LinkedList/deletion.cpp
+++ 10_LinkedList/deletion.cpp
@@ -6,3 +6,17 @@ Node* deleteHead(Node* head) {
     delete head; // Free memory of the old head or free(head) 
     return newHead; // Return the new head of the list
 }
+
+Node* deleteTail(Node* head){
+    if(head == NULL || head -> next == NULL){
+        return NULL;
+    }
+    Node* temp = head;
+    while(temp -> next -> next != NULL){
+        temp = temp -> next;
+    }
+    free(temp -> next); // delete temp -> next
+    temp -> next = nullptr;
+    
+    return head;
+}
