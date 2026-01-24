2fe91f3284adf5d0be2b84ddc0a92dc6575c0b5c
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-01-24 20:49:33 +0530

    add function to delete by position

--- 10_LinkedList/deletion.cpp
+++ 10_LinkedList/deletion.cpp
@@ -20,3 +20,28 @@ Node* deleteTail(Node* head){
     
     return head;
 }
+
+Node* deletePos(Node* head, int k){
+    if(head == NULL) return head; 
+    if(k == 1){
+        head = head -> next;
+        return head;
+    }
+    int cnt = 0; // tell us in which element we're  
+    int prev = NULL; // previous of 'cnt' element , set as null while cnt = 0
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cnt++;
+        if(cnt == k){
+            prev -> next = prev -> next -> next; // skiping one element for deleting
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp -> next;
+    }
+    
+    return head;
+}
+
