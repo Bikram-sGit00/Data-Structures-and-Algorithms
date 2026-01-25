2f0a5e7948045e86bc0d96e9d208b62a90edc17d
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-01-25 20:46:15 +0530

    add function to add element at tail

--- 10_LinkedList/02_insertion.cpp
+++ 10_LinkedList/02_insertion.cpp
@@ -2,3 +2,15 @@ Node* insertHead(Node* head,int val){
     return new Node(val,head); // (given_value, next)
 }
 
+Node* insertTail(Node* head, int val){
+    if(head == NULL) return new Node(val);
+    Node* temp = head;
+    while (temp -> next != NULL)
+    {
+        temp = temp -> next;
+    }
+    Node* newNode = new Node(val);
+    temp -> next = newNode;
+
+    return head;
+}
