e62dd4508580f60e07a4589528d307f653570fa5
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-01-31 20:10:45 +0530

    add function to delete the tail of a doubly linked list

--- 10_LinkedList/04_dllDeletion.cpp
+++ 10_LinkedList/04_dllDeletion.cpp
@@ -13,3 +13,18 @@ Node* deleteHead(Node* head){
 
 }
 
+Node* deleteTail(Node* head){
+    if(head == NULL || head -> next == NULL){
+        return NULL;
+    }
+    Node* tail = head;
+    while(tail -> next != NULL){
+        tail = tail -> next; // stoped at last node
+    }
+    node* prev = tail -> back ; // will give us second last node
+    tail -> back = nullptr; // disconnect last node from second last node
+    prev -> next = nullptr; // disconnect second last node from last node
+    
+    delete tail;
+    return head;
+}
