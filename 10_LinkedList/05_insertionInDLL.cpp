4c2bd9784b7c14e4b52a9d7e7a09f3e4aa41608c
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-02-01 12:14:29 +0530

    add function to insert a new node before the tail in a doubly linked list

--- 10_LinkedList/05_insertionInDLL.cpp
+++ 10_LinkedList/05_insertionInDLL.cpp
@@ -3,3 +3,18 @@ Node* insertBeforeHead(Node* head, int val){
     head -> prev = newHead;
     return newHead;
 }
+
+Node* insertBeforeTail(Node* head, int val){
+    if(head -> next == NULL){
+        return insertBeforeHead(head, val);
+    }
+    Node* tail = head; // find tail
+    while(tail -> next != NULL){
+        tail = tail -> next;
+    }
+    Node* prev = tail -> back;
+    Node* newNode = new Node(val, tail, prev); //(val, next, prev);
+    prev -> next = newNode;
+    tail -> back = newNode;
+    return head;
+}
