f138e07b41fe32f5ac04fb1670badd2a8d9c51ab
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-02-14 00:33:58 +0530

    add optimal solution of sorting linkedList

--- 10_LinkedList/02_MediumOf_LL/10_sortLL.cpp
+++ 10_LinkedList/02_MediumOf_LL/10_sortLL.cpp
@@ -33,3 +33,61 @@ public:
 
 // Time Complexity:O(2*N + NlogN)
 // Space Complexity:O(N)
+
+✅ Optimized Approach --> ListNode* findMiddle(ListNode* head){
+    ListNode* slow = head;
+    ListNode* fast = head -> next;
+    while(fast != NULL && fast -> next != NULL){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    }
+    return slow;
+}
+
+ListNode* merge(ListNode* left, ListNode* right){
+    ListNode* dummyNode = new Node(-1);
+    ListNode* temp = dummyNode;
+    while(list1 != NULL && list2 != NULL){
+        if(list1 -> data < list2 -> data){
+            temp-> next = list1;
+            list1 = list1 -> next;
+        }
+        else{
+            temp -> next = list2;
+            list2 = list2 -> next;
+        }
+        temp = temp -> next;
+    }
+    if(list1 != NULL){
+        temp -> next = list1;
+    }
+    else{
+        temp -> next = list2;
+    }
+    return dummyNode -> next;
+
+}
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(head== NULL || head=-> next==NULL){
+            return head;
+        }
+        ListNode* middleNode = findMiddle(head);
+        ListNode* right = middleNode -> next;
+        middleNode -> next = NULL;
+        ListNode* left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return merge(left,right);
+        
+    }
+};
+
+// Time Complexity: O(NlogN)
+// Space Complexity: O(1) (excluding recursive stack space)
+
+✅ Company Tags -->  Microsoft
