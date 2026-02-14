8473b610e433d46ed859ceeb3864c596b1c912f9
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-02-14 21:13:24 +0530

    add brute answer for sorting 0s 1s and 2s

---
+++ 10_LinkedList/02_MediumOf_LL/11_sort01&2s.cpp
@@ -0,0 +1,10 @@
+➡️ problemLinks --> https://www.geeksforgeeks.org/problems/given-a-linked-list-of-0s-1s-and-2s-sort-it/1
+
+✅ Brute Force -->  will set 3 counters for 0s, 1s and 2s and then will traverse the linked list and count the number of 0s, 1s and 2s. After that we will again traverse the linked list and update the values of nodes according to the count of 0s, 1s and 2s.
+
+// Time Complexity: O(N) + O(N) = O(2N) ~ O(N)
+// Space Complexity: O(1)
+
+✅ Optimized Approach --> 
+
+✅ Company Tags -->  
