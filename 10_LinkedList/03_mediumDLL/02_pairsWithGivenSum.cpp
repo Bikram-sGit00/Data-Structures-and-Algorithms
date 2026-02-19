2ad79c0e28f4223dc9af247f7ab5b4f7233fb741
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-02-19 12:33:04 +0530

    add optimized implementation for finding pairs with a given sum in a doubly linked list

--- 10_LinkedList/03_mediumDLL/02_pairsWithGivenSum.cpp
+++ 10_LinkedList/03_mediumDLL/02_pairsWithGivenSum.cpp
@@ -26,5 +26,35 @@ public:
 // Time Complexity: O(n^2) ,near about 
 // Space Complexity: O(1)
 
+✅ Optimized Approach --> Node* findTail(Node *head){
+    Node* tail = head;
+    while(tail -> next != NULL){
+        tail = tail -> next;
+    }
+    return tail;
+}
+
+class Solution {
+  public:
+    vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target) {
+        vector<pair<int, int>> ans;
+        if(head == NULL) return ans;
+        Node* left = head;
+        Node* right = findTail(head); // O(n)
+        while(left -> data < right -> data){ // (n)
+            if(left -> data + right -> data == target){
+                ans.push_back({left-> data , right -> data});
+                left = left -> next;
+                right = right -> prev;
+            }
+            else if (left -> data + right -> data < target) left = left -> next;
+            else right = right -> prev;
+        }
+        return ans;
+    }
+};
+
+// Time Complexity : O(2n)
+// Space Complexity : O(1) in order to store the ans;
 
 ✅ Company Tags -->  
