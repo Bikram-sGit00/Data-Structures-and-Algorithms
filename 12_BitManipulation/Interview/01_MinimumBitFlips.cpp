6ac4ba51f6b3866a254e9bb4b6d1269cb265e13d
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-07-25 00:42:27 +0530

    add implementation of Hamming Distance calculation using optimized approach

--- 12_BitManipulation/Interview/01_MinimumBitFlips.cpp
+++ 12_BitManipulation/Interview/01_MinimumBitFlips.cpp
@@ -17,3 +17,35 @@ public:
 // Time Complexity: O(k) , where k is the number of set bits
 // worst case : O(log n)
 // Space Complexity: O(1)
+
+
+✅ Similar Question --> Hamming Distance : https://leetcode.com/problems/hamming-distance/description/
+
+• Leetcode version can run on same code.
+
+• But in GFG version the input was given in an array.
+
+✅ Optimized Approach --> int totHammingDist(vector<int>& arr) {
+
+    int n = arr.size();
+    int ans = 0;
+
+    for(int bit = 0; bit < 32; bit++) {
+
+        int ones = 0;
+
+        for(int num : arr) {
+            if(num & (1 << bit))
+                ones++;
+        }
+
+        int zeros = n - ones;
+
+        ans += ones * zeros;
+    }
+
+    return ans;
+}
+//
+
+✅ Company Tags -->  Microsoft NPCI
