0b8a6f8bb3b69e0782b31262e19fd95d95970b31
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-08-17 21:27:38 +0530

    refactor optimized approach for Max Consecutive Ones III to improve clarity and efficiency

--- 14_SlidingWindow & TwoPointer/02_maxConsecutiveOnes3.cpp
+++ 14_SlidingWindow & TwoPointer/02_maxConsecutiveOnes3.cpp
@@ -21,14 +21,38 @@ public:
 }; 
 
 Time Complexity : O(n^2)
-
 Space Complexity : O(1)
 
-✅ Better Approach --> 
+✅ Better Approach --> class Solution { 
+public: 
+    int longestOnes(vector<int>& nums, int k) { 
+        int n = nums.size(); 
+        int l = 0;                  // left pointer of the window
+        int maxLen = 0;             // stores maximum valid window length
+        int zeroCnt = 0;            // counts zeros in the current window
 
-Time Complexity : 
+        for (int r = 0; r < n; r++) {   
+            if (nums[r] == 0) zeroCnt++; // include nums[r] in the window
 
-Space Complexity : 
+            while (zeroCnt > k) { // shrink until window becomes valid
+                if (nums[l] == 0) zeroCnt--; // remove zero when l moves and l == 0
+                l++; // move left pointer forward
+            }
+            // if (zeroCnt <= k) {
+            //     int len = r - l + 1;
+            //     maxLen = max(maxLen, len);
+            // } // No need of this as while() is already completed 
+
+            int len = r - l + 1; // calculate current valid window length
+            maxLen = max(maxLen, len); // update maximum length
+        } 
+
+        return maxLen; 
+    } 
+};
+
+Time Complexity : O(2N)
+Space Complexity : O(1)
 
 ✅ Optimized Approach --> 
 
