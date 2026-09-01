9ad5eea964a3932c9852ad96727f6bee1c67aa61
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-09-02 00:46:38 +0530

    add initial implementation for top K frequent elements with problem links and company tags

---
+++ 15_Heaps/03_topKFreq.cpp
@@ -0,0 +1,35 @@
+➡️ problemLinks --> https://leetcode.com/problems/top-k-frequent-elements/  &&  https://www.geeksforgeeks.org/problems/top-k-frequent-elements-in-array/1
+
+✅ Brute Force -->  
+
+Time Complexity : 
+
+Space Complexity : 
+
+✅ Better Approach --> 
+
+Time Complexity : 
+
+Space Complexity : 
+
+✅ Optimized Approach --> 
+
+Time Complexity : 
+
+Space Complexity : 
+
+✅ Company Tags -->  Amazon - asked 25 times in the last 6 months
+Google - asked 9 times in the last 6 months
+Bloomberg - asked 5 times in the last 6 months
+Meta - asked 4 times in the last 6 months
+Microsoft - asked 3 times in the last 6 months
+tcs - asked 3 times in the last 6 months
+Walmart Labs - asked 3 times in the last 6 months
+Nvidia - asked 2 times in the last 6 months
+American Express - asked 2 times in the last 6 months
+Apple - asked 2 times in the last 6 months
+TikTok - asked 2 times in the last 6 months
+SoFi - asked 2 times in the last 6 months
+Visa - asked 2 times in the last 6 months
+Snowflake - asked 2 times in the last 6 months
+eBay - asked 2 times in the last 6 months
