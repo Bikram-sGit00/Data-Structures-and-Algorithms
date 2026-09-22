a7e712935c9a9f4fbebed8707fd464643d70efdd
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-09-22 19:38:20 +0530

    add initial implementation for median of data stream with problem links and company tags

---
+++ 15_Heaps/12_medianOfDataStream.cpp
@@ -0,0 +1,26 @@
+➡️ problemLinks --> https://leetcode.com/problems/find-median-from-data-stream/  && https://www.geeksforgeeks.org/problems/find-median-in-a-stream-1587115620/1
+
+✅ Brute Force -->  
+
+Time Complexity : 
+
+Space Complexity : 
+
+
+✅ Optimized Approach --> 
+
+Time Complexity : 
+
+Space Complexity : 
+
+✅ Company Tags -->  Amazon - asked 9 times in the last 6 months
+Intuit - asked 4 times in the last 6 months
+Google - asked 3 times in the last 6 months
+Adobe - asked 3 times in the last 6 months
+Bloomberg - asked 3 times in the last 6 months
+Apple - asked 3 times in the last 6 months
+TikTok - asked 2 times in the last 6 months
+Spotify - asked 2 times in the last 6 months
+IXL - asked 2 times in the last 6 months
+DE Shaw - asked 2 times in the last 6 months
+Uber - asked 2 times in the last 6 months
