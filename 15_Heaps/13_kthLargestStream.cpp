6d609fee1c54da71ec37cba45338f293e8b16ec9
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-09-23 23:17:06 +0530

    add GFG version of KthLargest class for finding the kth largest element in a stream

--- 15_Heaps/13_kthLargestStream.cpp
+++ 15_Heaps/13_kthLargestStream.cpp
@@ -27,3 +27,32 @@ Time Complexity :: O(n log k) for constructor and O(log k) for add() function, e
 
 Space Complexity :: O(k) for storing k elements in the heap
 
+
+✅ GFG Version --> https://www.geeksforgeeks.org/problems/kth-largest-element-in-a-stream2220/1
+class Solution {
+	public:
+	vector<int> kthLargest(vector<int>& arr, int k) {
+		priority_queue<int, vector<int>, greater<int>> minHeap;
+		vector<int> ans;
+		for (auto it : arr) {
+			minHeap.push(it); // temporarily allow heap to extend k + 1 size
+			if (minHeap.size() > k) { // now again shrink to k , tha's how we will not loose anything, e.g
+				minHeap.pop(); // heap = [5, 8, 10], insert(2), Now heap = [2, 5, 8, 10], so 2 will get poped
+			}
+			if(minHeap.size() < k){
+			    ans.push_back(-1);
+			}
+			else ans.push_back(minHeap.top());
+			
+		}
+		return ans;
+	}
+};
+
+Time Complexity :: O(n log k) 
+
+Space Complexity :: O(k) auxiliary
+
+Output :: O(n)
+
+✅ Company Tags -->  Flipkart Amazon Microsoft OYORooms Hike Walmart Cisco Adobe
