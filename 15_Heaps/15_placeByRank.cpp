1b3bc66d9bd4063893a095e4a61a283f56fe3f6c
Bikram-sGit00 <ContactBikram1@gmail.com>
2026-09-24 21:50:18 +0530

    add optimized approach for array rank transformation using a min-heap

--- 15_Heaps/15_placeByRank.cpp
+++ 15_Heaps/15_placeByRank.cpp
@@ -1,4 +1,4 @@
-➡️ problemLinks -->  
+➡️ problemLinks -->  https://leetcode.com/problems/rank-transform-of-an-array/description/  &&  https://www.geeksforgeeks.org/problems/convert-an-array-to-reduced-form1101/1
 
 ✅ Optimized Approach 1 --> class Solution { 
 public: 
@@ -34,6 +34,39 @@ Time Complexity : O(n log n)
 
 Space Complexity : O(n)
 
+
+✅ Optimized Approach 2 --> class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for(int i = 0; i < arr.size(); i++){
+            minHeap.push(arr[i]);
+        }
+        unordered_map<int, int> mpp;
+        int rank = 1;
+        while(!minHeap.empty()){
+            // if(mpp.find(minHeap.top()) == mpp.end()){
+            //     mpp[minHeap.top()] = rank++;
+            //     minHeap.pop();
+            // }
+            // else minHeap.pop(); // also pop for equal element
+
+            int num = minHeap.top();
+            minHeap.pop();
+            if(mpp.find(num) == mpp.end()){
+                mpp[num] = rank++;
+            }
+            
+        }
+        for(int i = 0; i < arr.size(); i++){
+            arr[i] = mpp[arr[i]]; 
+        }
+        return arr;
+    }
+};
+
+Time Complexity : O(n log n)
+Space Complexity : O(n)
 
 
 ✅ Company Tags -->  
